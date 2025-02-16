33046602973edb3b513a745c4ed196c88594e71a
Andrew Emmanuel Alonso Valenzuela <andrew.alonso@citi.com.mx>
2025-02-16 11:54:14 -0600

    Actualizacion en postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
 // postcss.config.js
-module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  }
-  
+import tailwindcss from '@tailwindcss/postcss7-compat';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
+  ],
+};
